19406946b5b81075147a9b169e063c4964e96a56
William <will.h86@gmail.com>
2016-12-04 08:13:05 -0800

    sourcemaps for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // NOTE: paths are relative to each functions folder
-// const webpack = require('webpack');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -29,4 +29,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin(),
+  ],
 };
