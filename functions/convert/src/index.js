c8a5aacb27628c9b398c035e409907cc9d476082
William <will.h86@gmail.com>
2016-12-04 15:15:43 -0800

    test function

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -1,22 +1,32 @@
 import λ from 'apex.js';
 import phantom from 'phantom';
+import { createReadStream, statSync } from 'fs';
+import template from './lib/template';
 
 Promise.coroutine.addYieldHandler(value => Promise.resolve(value));
 
-export default λ(async ({ html, css }) => {
+export default λ(async ({ filename, html, css }) => {
   console.log('html', html);
   console.log('css', css);
+  console.log('filename', filename);
   const instance = await phantom.create();
   const page = await instance.createPage();
-  await page.on('onResourceRequested', (data) => {
-      console.info('Requesting', data.url);
+
+  page.property('paperSize', {
+    format: 'A4',
+    orientation: 'landscape',
   });
 
-  const status = await page.open('https://stackoverflow.com/');
-  console.log(status);
-
-  const content = await page.property('content');
-  console.log(content);
+  page.property('viewportSize', {
+    width: 1056,
+    height: 816,
+  });
+  page.set('property', template({ html, css }));
+  await page.render(filename, { format: 'pdf', quality: 100 });
   await instance.exit();
-  return content;
+
+  const file = createReadStream(filename);
+  const stat = statSync(filename);
+  console.log('fileSize', stat.size);
+  return file;
 });
