0842ca41560684c07a0ddcae0ec31127fc963d9d
William <will.h86@gmail.com>
2016-12-04 15:39:16 -0800

    more logs for debugging

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -21,11 +21,17 @@ export default λ(async ({ filename, html, css }) => {
     width: 1056,
     height: 816,
   });
+
   page.property('content', template({ html, css }));
 
+  const content = await page.property('content');
+
   try {
+    console.log('rendering', content)
     await page.render(filename);
   } catch (e) {
+    console.log('error in render', e);
+    console.log('error rendering', content);
     return e;
   } finally {
     await instance.exit();
