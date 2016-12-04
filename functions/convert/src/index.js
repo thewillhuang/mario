315cb18f21bf6ca2e5e762b330c7616181856a33
William <will.h86@gmail.com>
2016-12-04 08:41:47 -0800

    quick example

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -1,6 +1,21 @@
 import λ from 'apex.js';
 import phantom from 'phantom';
 
-export default λ(async ({ html, css }) =>
-  await phantom
-    .create(['--ignore-ssl-errors=yes', '--load-images=no']));
+export default λ(async ({ html, css }) => {
+  console.log('html', html);
+  console.log('css', css);
+  console.log('phantom', phantom);
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  await page.on("onResourceRequested", function (data) {
+      console.info('Requesting', data.url);
+  });
+
+  const status = await page.open('https://stackoverflow.com/');
+  console.log(status);
+
+  const content = await page.property('content');
+  console.log(content);
+
+  await instance.exit();
+});
