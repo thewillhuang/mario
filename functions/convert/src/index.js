e214da5604a777fd807cc0e9ef8e5cdd5ff6f31a
William <will.h86@gmail.com>
2016-12-03 21:16:09 -0800

    use current node version

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -1,9 +1,4 @@
 import λ from 'apex.js';
 import phantom from 'phantom';
 
-export default λ((e) => {
-  console.log(e);
-  return phantom
-    .create(['--ignore-ssl-errors=yes', '--load-images=no'])
-    .then(res => res);
-});
+export default λ(async e => await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']));
