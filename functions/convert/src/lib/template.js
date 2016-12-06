e8bf4a9c7854f5af26e38f87615a05891be899a5
William <will.h86@gmail.com>
2016-12-05 23:22:55 -0800

    linux fix, util file

--- functions/convert/src/lib/template.js
+++ functions/convert/src/lib/template.js
@@ -1,25 +1,11 @@
-const generateLinkTag = (cssUrl) => {
-  let css = '';
-  cssUrl.forEach((url) => {
-    css += `<link href="${url}" rel="stylesheet">`;
-  });
-  return css;
-};
-
-const generateScriptTag = (jsUrl) => {
-  let js = '';
-  jsUrl.forEach((url) => {
-    js += `<script src="${url}"></script>`;
-  });
-  return js;
-};
+import { generateLinkTag, generateScriptTag, phantom2LinuxViewportStyle } from './utils';
 
 export default ({ html, css, cssUrls, jsUrls }) =>
   `<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <style>body { transform-origin: 0 0; -webkit-transform-origin: 0 0; transform: scale(0.654545); -webkit-transform: scale(0.654545); }</style>
+    ${phantom2LinuxViewportStyle}
     ${generateLinkTag(cssUrls)}
     <style>${css}</style>
   </head>
---
+++ functions/convert/src/lib/utils.js
@@ -0,0 +1,17 @@
+export const generateLinkTag = (cssUrl) => {
+  let css = '';
+  cssUrl.forEach((url) => {
+    css += `<link href="${url}" rel="stylesheet">`;
+  });
+  return css;
+};
+
+export const generateScriptTag = (jsUrl) => {
+  let js = '';
+  jsUrl.forEach((url) => {
+    js += `<script src="${url}"></script>`;
+  });
+  return js;
+};
+
+export const phantom2LinuxViewportStyle = '<style>body { transform-origin: 0 0; -webkit-transform-origin: 0 0; transform: scale(0.654545); -webkit-transform: scale(0.654545); }</style>';
