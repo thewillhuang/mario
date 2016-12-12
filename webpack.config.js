9ada3c44b31260c7b15c84de4be99cffc04b5005
William <will.h86@gmail.com>
2016-12-12 10:29:44 -0800

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,6 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
     ],
   },
   plugins: [
