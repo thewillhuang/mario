6f73fd75ede21e11b25484520bea92d331dc2f14
William Huang <will.h86@gmail.com>
2017-04-18 19:02:13 -0700

    update

---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+// NOTE: paths are relative to each functions folder
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  entry: ['./src/index.js'],
+  target: 'node',
+  output: {
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: ['aws-sdk', 'phantom'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // options: {
+        //   cacheDirectory: true,
+        // },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /babel-runtime\/core-js\/promise/,
+      'bluebird',
+    ),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: { comments: false },
+    }),
+  ],
+};
