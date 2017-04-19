08a151df3808323c7cb0c149242a255d27a49e1d
William Huang <will.h86@gmail.com>
2017-04-19 01:50:03 -0700

    upload to s3

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -1,11 +1,16 @@
 import λ from 'apex.js';
 import phantom from 'phantom';
 import { v4 as uuid } from 'uuid';
+import { S3, config } from 'aws-sdk';
 
 import fs from './lib/fs';
 import template from './lib/template';
 
-const { readFileAsync, unlinkAsync } = fs;
+const { createReadStream, unlinkAsync } = fs;
+
+console.log(S3, config);
+
+config.setPromisesDependency(global.Promise);
 
 const cleanup = async (instance, filePath) => {
   // kill phantom js process
@@ -15,17 +20,18 @@ const cleanup = async (instance, filePath) => {
 
 export default λ(async ({
   ping,
-  html = '<p>no html content</p>',
+  html = '',
   js = '',
   css = '',
   cssUrls = [],
   jsUrls = [],
   pageConfig,
 }) => {
+  console.time('duration');
   // heartbeat
-  if (ping) { return 'heartbeat'; }
+  if (ping) { return { message: 'ack' }; }
 
-  // lambda only gives permission on /tmp/
+  // lambda only gives write permission on /tmp/
   const filePath = `/tmp/${uuid()}.pdf`;
 
   // setup phantom
@@ -34,7 +40,7 @@ export default λ(async ({
 
   try {
     // sets page property
-    Object.keys(pageConfig).forEach(config => page.property(config, pageConfig[config]));
+    Object.keys(pageConfig).forEach(options => page.property(options, pageConfig[options]));
 
     // sets content for phantom to render
     page.property('content', template({ html, css, js, cssUrls, jsUrls }));
@@ -43,13 +49,17 @@ export default λ(async ({
     await page.render(filePath);
 
     // read the pdf as base64
-    const content = await readFileAsync(filePath, { encoding: 'base64' });
+
+    const upload = new S3.ManagedUpload({
+      params: { Bucket: 'pdf-upload-test', Key: uuid(), Body: createReadStream(filePath) },
+    });
+
+    const { Location } = await upload.promise();
 
     // clean up
     cleanup(instance, filePath);
-
-    // return for user content as base64 and let Api Gateway convert to binary
-    return content;
+    console.timeEnd('duration');
+    return { url: Location };
   } catch (e) {
     console.log('error', e);
     // cleanup
