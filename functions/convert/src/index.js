87c904efd1658ae0cf09f1a4667e52fa0d30e7b6
William Huang <will.h86@gmail.com>
2017-04-19 10:00:32 -0700

    typo

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -32,7 +32,7 @@ export default λ(async ({
   if (ping) { return { message: 'ack' }; }
 
   // lambda only gives write permission on /tmp/
-  const fileName = `${uuid().pdf}`;
+  const fileName = `${uuid()}.pdf`;
   const filePath = `/tmp/${fileName}`;
 
   // setup phantom
@@ -52,6 +52,7 @@ export default λ(async ({
     const Body = createReadStream(filePath);
     const ContentDisposition = contentDisposition(filePath);
     const ContentType = mime.lookup(filePath);
+
     const params = {
       Bucket: 'mario-pdf-upload',
       Key: uuid(),
