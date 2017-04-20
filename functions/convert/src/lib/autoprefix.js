d96da8c21709e3fa2eedb6d82c8117c581f41071
William Huang <will.h86@gmail.com>
2017-04-19 17:07:38 -0700

    fix warnings

--- functions/convert/src/lib/autoprefix.js
+++ functions/convert/src/lib/autoprefix.js
@@ -2,11 +2,14 @@ import autoprefixer from 'autoprefixer';
 import postcss from 'postcss';
 
 export default async (css) => {
+  // eslint-disable-next-line
   console.time('autoprefix time');
   const result = await postcss([autoprefixer]).process(css);
   result.warnings().forEach((warn) => {
+    // eslint-disable-next-line
     console.warn(warn.toString());
   });
+  // eslint-disable-next-line
   console.timeEnd('autoprefix time');
   return result.css;
 };
