fc91dfd2dec5b813eb419d0176af80b912e39a12
William Huang <will.h86@gmail.com>
2017-10-07 21:18:49 -0700

    spacing

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -4,7 +4,6 @@ import { generatePdfWithRawContent } from './lib/generate';
 import { getFromS3, uploadToS3 } from './lib/s3Helpers';
 
 let browser;
-
 export default λ(async ({ Records }) => {
   const { s3: { object: { key }, bucket: { name: srcBucket } } } = Records[0];
   const destBucket = `${srcBucket}-pdf`;
