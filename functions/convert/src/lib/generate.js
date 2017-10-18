e87f3f00706fa9dce4172359bff5d7030d4a540a
William Huang <will.h86@gmail.com>
2017-10-18 11:22:13 -0700

    try catch block

--- functions/convert/src/lib/generate.js
+++ functions/convert/src/lib/generate.js
@@ -1,19 +1,26 @@
 import template from './template';
 
 export const generatePdfWithRawContent = async (browser, content) => {
-  console.log('generating raw content');
-  const page = await browser.newPage();
-  page.setContent(template({ content }));
-  const pdf = await page.pdf({ format: 'A4', landscape: true });
-  console.log(pdf);
-  await page.close();
-  return pdf;
+  try {
+    console.log('generating raw content');
+    const page = await browser.newPage();
+    page.setContent(template({ content }));
+    const pdf = await page.pdf({ format: 'A4', landscape: true });
+    await page.close();
+    return pdf;
+  } catch (e) {
+    console.log('generatePdfWithRawContent error', e);
+  }
 };
 
 export const generatePdfWithUrl = async (browser, url) => {
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle' });
-  const pdf = await page.pdf({ format: 'A4', landscape: true });
-  await page.close();
-  return pdf;
+  try {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle' });
+    const pdf = await page.pdf({ format: 'A4', landscape: true });
+    await page.close();
+    return pdf;
+  } catch (e) {
+    console.log('generatePdfWithUrl error', e);
+  }
 };
