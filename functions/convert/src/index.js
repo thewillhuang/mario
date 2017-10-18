8a8eb32e1fb4faa481a2aff5054268e67da66e2b
William Huang <will.h86@gmail.com>
2017-10-17 17:07:43 -0700

    test

--- functions/convert/src/index.js
+++ functions/convert/src/index.js
@@ -1,7 +1,7 @@
 import λ from 'apex.js';
-import getBrowser from './lib/setup';
-import { generatePdfWithRawContent } from './lib/generate';
-import { getFromS3, uploadToS3 } from './lib/s3Helpers';
+// import getBrowser from './lib/setup';
+// import { generatePdfWithRawContent } from './lib/generate';
+// import { getFromS3, uploadToS3 } from './lib/s3Helpers';
 
 let browser;
 export default λ(async ({ Records }) => {
@@ -9,21 +9,23 @@ export default λ(async ({ Records }) => {
   const { s3: { object: { key }, bucket: { name: srcBucket } } } = Records[0];
   const destBucket = `${srcBucket}-pdf`;
 
-  console.time('grab content from s3');
-  const content = JSON.parse(await getFromS3(srcBucket, key));
-  console.timeEnd('grab content from s3');
+  console.log('key src bucket', key, srcBucket, destBucket);
 
-  console.time('generate pdf');
-  let pdfBuffer;
-  if (browser) {
-    pdfBuffer = generatePdfWithRawContent(browser, content);
-  } else {
-    browser = await getBrowser();
-    pdfBuffer = generatePdfWithRawContent(browser, content);
-  }
-  console.timeEnd('generate pdf');
+  // console.time('grab content from s3');
+  // const content = JSON.parse(await getFromS3(srcBucket, key));
+  // console.timeEnd('grab content from s3');
 
-  console.timeEnd('upload pdf to destination bucket');
-  await uploadToS3(destBucket, key, pdfBuffer);
-  console.timeEnd('upload pdf to destination bucket');
+  // console.time('generate pdf');
+  // let pdfBuffer;
+  // if (browser) {
+  //   pdfBuffer = generatePdfWithRawContent(browser, content);
+  // } else {
+  //   browser = await getBrowser();
+  //   pdfBuffer = generatePdfWithRawContent(browser, content);
+  // }
+  // console.timeEnd('generate pdf');
+
+  // console.timeEnd('upload pdf to destination bucket');
+  // await uploadToS3(destBucket, key, pdfBuffer);
+  // console.timeEnd('upload pdf to destination bucket');
 });
