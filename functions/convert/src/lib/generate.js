8bdffc7e336a054b12d68b3359db31bbbf78b6e4
William Huang <will.h86@gmail.com>
2017-10-18 10:53:51 -0700

    async functions always returns a promise

--- functions/convert/src/lib/generate.js
+++ functions/convert/src/lib/generate.js
@@ -1,26 +1,19 @@
 import template from './template';
 
-export const generatePdfWithRawContent = (browser, content) => new Promise(async (resolve, reject) => {
-  try {
-    console.log('generating raw content');
-    const page = await browser.newPage();
-    page.setContent(template({ content }));
-    const pdf = await page.pdf({ format: 'A4', landscape: true });
-    await page.close();
-    resolve(pdf);
-  } catch (e) {
-    reject(e);
-  }
-});
+export const generatePdfWithRawContent = async (browser, content) => {
+  console.log('generating raw content');
+  const page = await browser.newPage();
+  page.setContent(template({ content }));
+  const pdf = await page.pdf({ format: 'A4', landscape: true });
+  console.log(pdf);
+  await page.close();
+  return pdf;
+};
 
-export const generatePdfWithUrl = (browser, url) => new Promise(async (resolve, reject) => {
-  try {
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle' });
-    const pdf = await page.pdf({ format: 'A4', landscape: true });
-    await page.close();
-    resolve(pdf);
-  } catch (e) {
-    reject(e);
-  }
-});
+export const generatePdfWithUrl = async (browser, url) => {
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle' });
+  const pdf = await page.pdf({ format: 'A4', landscape: true });
+  await page.close();
+  return pdf;
+};
