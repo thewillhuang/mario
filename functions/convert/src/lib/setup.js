3d67f70d11a9cd6d466c2740552874a40cbd1e34
William Huang <will.h86@gmail.com>
2017-10-24 00:12:58 -0700

    log tmp

--- functions/convert/src/lib/setup.js
+++ functions/convert/src/lib/setup.js
@@ -1,36 +1,32 @@
 import gunzip from 'gunzip-maybe';
-import tar from 'tar';
+import tar from 'tar-fs';
 import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 
-// const setupLocalChrome = () => {
-//   fs.createReadStream(path.join(process.cwd(), './headless_shell.tar.gz'))
-//     .pipe(gunzip())
-//     .pipe(tar.extract(path.join(__dirname, './tmp')));
-//   console.log(path.join(__dirname, './tmp'));
-//   console.log(fs.readdirSync(path.join(__dirname, './tmp/')));
-//   console.log(fs.readdirSync('/tmp'));
-// };
-
-const setupLocalChrome = () => new Promise((resolve, reject) => {
+const setupLocalChrome = () => {
   fs.createReadStream(path.join(process.cwd(), './headless_shell.tar.gz'))
-    .on('error', err => reject(err))
-    .pipe(tar.x({
-      C: path.join(path.sep, 'tmp'),
-    }))
-    .on('error', err => reject(err))
-    .on('end', () => resolve());
-});
+    .pipe(gunzip())
+    .pipe(tar.extract('/tmp/'));
+  console.log(fs.readdirSync('/tmp'));
+};
+
+// const setupLocalChrome = () => new Promise((resolve, reject) => {
+//   fs.createReadStream(path.join(process.cwd(), './headless_shell.tar.gz'))
+//     .on('error', err => reject(err))
+//     .pipe(tar.x({
+//       C: path.join(path.sep, 'tmp'),
+//     }))
+//     .on('error', err => reject(err))
+//     .on('end', () => resolve());
+// });
 
 const getBrowser = async (browser) => {
   try {
     if (browser !== false) {
       return browser;
     }
-    setupLocalChrome().then(() => {
-      console.log(fs.readdirSync('/tmp'));
-    }).catch((e) => { console.log(e); return e; });
+    setupLocalChrome();
     return await puppeteer.launch({
       headless: true,
       executablePath: path.join(__dirname, '/tmp/headless_shell'),
