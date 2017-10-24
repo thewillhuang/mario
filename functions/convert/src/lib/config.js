ab91615819f1b230d71f5d145079c3e662b7560f
William Huang <will.h86@gmail.com>
2017-10-23 17:18:17 -0700

    update

--- functions/convert/src/lib/config.js
+++ functions/convert/src/lib/config.js
@@ -13,7 +13,7 @@ export const localChromePath = path.join('headless_shell.tar.gz');
 export const remoteChromeS3Bucket = process.env.CHROME_BUCKET;
 export const remoteChromeS3Key = process.env.CHROME_KEY || 'headless_shell.tar.gz';
 
-export const setupChromePath = path.join(path.sep, 'tmp');
+const setupChromePath = path.join(path.sep, 'tmp');
 export const executablePath = path.join(
     setupChromePath,
     'headless_shell',
